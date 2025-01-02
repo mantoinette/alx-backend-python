2dff3391e3350486e9369e6aa47b8557197714f9
mantoinette <antoinettemushimiyimana163@gmail.com>
2025-01-02 23:26:26 +0200

    debuging same errors

--- chats/views.py
+++ chats/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def chat_list(request):
+    chats = [
+        {"id": 1, "message": "Hello!"},
+        {"id": 2, "message": "How are you?"},
+    ]
+    return JsonResponse(chats, safe=False)
