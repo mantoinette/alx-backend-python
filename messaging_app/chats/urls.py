6fea9ba3c62eb4b5310be93b568f59d857204a78
mantoinette <antoinettemushimiyimana163@gmail.com>
2025-01-27 22:09:25 +0200

    working on the same error in the url file

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,10 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
+# Use DefaultRouter to register viewsets
 router = DefaultRouter()
 router.register(r"conversations", ConversationViewSet, basename="conversation")
 router.register(r"messages", MessageViewSet, basename="message")
 
+# Include the router's URLs in the urlpatterns
 urlpatterns = [
-    path("", include(router.urls)),
+    path("", include(router.urls)),  # Register routes from the router
 ]
