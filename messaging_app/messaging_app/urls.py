ffc25dbe39d19029387f6e08ef0e51db2070c72d
mantoinette <antoni.kigali@gmail.com>
2025-02-18 16:48:12 -0800

    Add JWT authentication implementation

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("chats.urls")),  # Make sure this line is added to include the chats app's URLs
     path('api-auth/', include('rest_framework.urls')),  # Add this line
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
