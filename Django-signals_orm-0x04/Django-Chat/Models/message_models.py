74b09f0fcae8e5906e86e7ecf1675532ec54ca67
mantoinette <antoni.kigali@gmail.com>
2025-02-19 21:11:08 -0800

    working on same file

--- Django-signals_orm-0x04/Django-Chat/Models/message_models.py
+++ Django-signals_orm-0x04/Django-Chat/Models/message_models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from mptt.models import MPTTModel, TreeForeignKey
 
-class Message(models.Model):
+class Message(MPTTModel):
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sent_messages')
     receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='received_messages')
     content = models.TextField()
@@ -10,9 +11,52 @@ class Message(models.Model):
     is_read = models.BooleanField(default=False)
     edited = models.BooleanField(default=False)  # Track if message has been edited
     last_edited = models.DateTimeField(null=True, blank=True)  # When it was last edited
+    
+    # Add parent_message field for threaded conversations
+    parent = TreeForeignKey(
+        'self',
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        related_name='replies'
+    )
+
+    class MPTTMeta:
+        order_insertion_by = ['timestamp']
 
     def __str__(self):
         return f"From {self.sender} to {self.receiver}"
+
+    @property
+    def thread_messages(self):
+        """Get all messages in the thread efficiently"""
+        return (
+            Message.objects
+            .filter(tree_id=self.tree_id)
+            .select_related('sender', 'receiver')
+            .prefetch_related('history')
+            .order_by('tree_id', 'lft')
+        )
+
+    """
+    Example usage of threaded messages:
+    
+    # Get a message and all its replies efficiently
+    message = Message.objects.get(id=1)
+    thread = message.thread_messages
+
+    # Get direct replies to a message
+    direct_replies = message.replies.all()
+
+    # Get the parent message
+    parent = message.parent
+
+    # Get the root message of a thread
+    root_message = message.get_root()
+
+    # Get all descendants (replies) of a message
+    all_replies = message.get_descendants()
+    """
 
 class MessageHistory(models.Model):
     message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='history')
