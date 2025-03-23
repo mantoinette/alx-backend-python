2d3b2060c4bdf0886c70a5a1a221368ca9bbe110
mantoinette <152092675+mantoinette@users.noreply.github.com>
2025-03-23 02:54:29 -0700

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -13,19 +13,14 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google"),
         ("abc"),
     ])
-    @patch('client.GithubOrgClient.get_json')
-    def test_org(self, org_name, mock_get_json):
+    def test_org(self, org_name):
         """Test that GithubOrgClient.org returns the correct value"""
-        test_class = GithubOrgClient(org_name)
-        # Configure mock to return a specific value
-        mock_get_json.return_value = {"payload": True}
-        result = test_class.org()
-        # Check if get_json was called with the correct URL
-        mock_get_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{org_name}"
-        )
-        # Check if the result matches the mock return value
-        self.assertEqual(result, {"payload": True})
+        with patch('client.get_json') as mock_json:
+            test_class = GithubOrgClient(org_name)
+            test_class.org()
+            mock_json.assert_called_once_with(
+                f"https://api.github.com/orgs/{org_name}"
+            )
 
 
 if __name__ == '__main__':
