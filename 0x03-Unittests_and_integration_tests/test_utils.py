ea2270905123ceb6eeba288871f3b405f5521719
mantoinette <152092675+mantoinette@users.noreply.github.com>
2025-03-23 02:38:00 -0700

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 """Test module for utils.memoize decorator"""
-
 import unittest
 from unittest.mock import patch
 from utils import memoize
@@ -8,7 +7,6 @@ from utils import memoize
 
 class TestMemoize(unittest.TestCase):
     """Testing memoization"""
-
     def test_memoize(self):
         """Test that when calling a_property twice, the correct result
         is returned but a_method is only called once using assert_called_once
@@ -30,4 +28,3 @@ class TestMemoize(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-    
