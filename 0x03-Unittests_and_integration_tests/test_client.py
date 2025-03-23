3f9b33886205350a25b4187d2a240ccd0790753c
mantoinette <152092675+mantoinette@users.noreply.github.com>
2025-03-23 07:16:56 -0700

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """Test module for GithubOrgClient class"""
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 from parameterized import parameterized
 from client import GithubOrgClient
 
@@ -13,16 +13,32 @@ class TestGithubOrgClient(unittest.TestCase):
         ("google",),
         ("abc",),
     ])
-    @patch('client.get_json')
-    def test_org(self, org_name, mock_get):
+    def test_org(self, org_name):
         """Test that GithubOrgClient.org returns the correct value"""
-        test_client = GithubOrgClient(org_name)
-        mock_get.return_value = {"name": org_name}
-        result = test_client.org()
-        self.assertEqual(result, {"name": org_name})
-        mock_get.assert_called_once_with(
-            f"https://api.github.com/orgs/{org_name}"
-        )
+        with patch('client.get_json') as mock_json:
+            test_client = GithubOrgClient(org_name)
+            mock_json.return_value = {"name": org_name}
+            self.assertEqual(test_client.org(), {"name": org_name})
+            mock_json.assert_called_once_with(
+                f"https://api.github.com/orgs/{org_name}"
+            )
+
+    def test_public_repos_url(self):
+        """Test that the result of _public_repos_url is the expected one
+        based on the mocked payload
+        """
+        with patch(
+            'client.GithubOrgClient.org',
+            new_callable=PropertyMock
+        ) as mock_org:
+            mock_org.return_value = {
+                'repos_url': "https://api.github.com/orgs/google/repos"
+            }
+            test_client = GithubOrgClient("google")
+            self.assertEqual(
+                test_client._public_repos_url,
+                "https://api.github.com/orgs/google/repos"
+            )
 
 
 if __name__ == '__main__':
