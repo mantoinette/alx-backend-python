1eb240d0533432111bd63e19514620264a1f443a
mantoinette <152092675+mantoinette@users.noreply.github.com>
2025-03-24 06:10:26 -0700

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,7 +3,7 @@
 import unittest
 from unittest.mock import patch, PropertyMock
 from utils import get_json, access_nested_map
-from parameterized import parameterized
+from parameterized import parameterized, parameterized_class
 from client import GithubOrgClient
 import utils  # Ensure utils is properly imported
 
