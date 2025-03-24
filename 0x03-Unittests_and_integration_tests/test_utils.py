ebbd7509d60b202c13f76629b1313e9e9d914da8
mantoinette <152092675+mantoinette@users.noreply.github.com>
2025-03-24 05:57:56 -0700

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,29 +1,21 @@
 #!/usr/bin/env python3
-"""Test module for utils.memoize decorator"""
+"""Test module for utils.py"""
 import unittest
 from unittest.mock import patch
-from utils import memoize
+import utils
 
 
-class TestMemoize(unittest.TestCase):
-    """Testing memoization"""
-    def test_memoize(self):
-        """Test that when calling a_property twice, the correct result
-        is returned but a_method is only called once using assert_called_once
-        """
-        class TestClass:
-            def a_method(self):
-                return 42
+class TestUtils(unittest.TestCase):
+    """Test class for utils"""
 
-            @memoize
-            def a_property(self):
-                return self.a_method()
+    @patch('utils.requests.get')
+    def test_get_json(self, mock_get):
+        """Test get_json function"""
+        mock_get.return_value.json.return_value = {"key": "value"}
 
-        with patch.object(TestClass, 'a_method') as mock_method:
-            test_class = TestClass()
-            test_class.a_property()
-            test_class.a_property()
-            mock_method.assert_called_once()
+        result = utils.get_json("http://example.com")
+        mock_get.assert_called_once_with("http://example.com")
+        self.assertEqual(result, {"key": "value"})
 
 
 if __name__ == '__main__':
