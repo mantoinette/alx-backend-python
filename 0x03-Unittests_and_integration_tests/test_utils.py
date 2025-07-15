bc03c581dba4be710ca7a791dd50818b64ea7d5c
mantoinette <152092675+mantoinette@users.noreply.github.com>
2025-07-15 13:28:39 +0200

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,28 +5,19 @@ from utils import access_nested_map, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    """Test class for access_nested_map"""
-
     def test_access_nested_map(self):
-        """Test normal cases"""
         self.assertEqual(access_nested_map({"a": {"b": 2}}, ["a", "b"]), 2)
         self.assertEqual(access_nested_map({"x": {"y": {"z": 5}}}, ["x", "y", "z"]), 5)
 
     def test_access_nested_map_key_error(self):
-        """Test KeyError when path is missing"""
         with self.assertRaises(KeyError):
             access_nested_map({}, ["a"])
-
         with self.assertRaises(KeyError):
             access_nested_map({"a": 1}, ["a", "b"])
 
 
 class TestMemoize(unittest.TestCase):
-    """Test class for the memoize decorator"""
-
     def test_memoize(self):
-        """Test memoization"""
-
         class TestClass:
             def __init__(self):
                 self.counter = 0
@@ -38,14 +29,11 @@ class TestMemoize(unittest.TestCase):
 
         obj = TestClass()
 
-        # First call should increase counter
-        result1 = obj.method
-        # Second call should return the cached result
-        result2 = obj.method
-
-        self.assertEqual(result1, 1)
-        self.assertEqual(result2, 1)
-        self.assertEqual(obj.counter, 1)  # Confirm method was called only once
+        val1 = obj.method
+        val2 = obj.method
+        self.assertEqual(val1, 1)
+        self.assertEqual(val2, 1)
+        self.assertEqual(obj.counter, 1)  # method ran once and result was cached
 
 
 if __name__ == '__main__':
