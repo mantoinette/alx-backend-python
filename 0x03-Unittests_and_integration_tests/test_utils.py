b8c35982e93a962f8b4c55f5ce5ff270b14a298d
mantoinette <152092675+mantoinette@users.noreply.github.com>
2025-07-15 11:45:23 +0200

    Update test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """Test module for utils.py"""
 import unittest
-from utils import access_nested_map
-from utils import memoize
-
+from utils import access_nested_map, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -21,6 +19,33 @@ class TestAccessNestedMap(unittest.TestCase):
 
         with self.assertRaises(KeyError):
             access_nested_map({"a": 1}, ["a", "b"])
+
+
+class TestMemoize(unittest.TestCase):
+    """Test class for the memoize decorator"""
+
+    def test_memoize(self):
+        """Test memoization"""
+
+        class TestClass:
+            def __init__(self):
+                self.counter = 0
+
+            @memoize
+            def method(self):
+                self.counter += 1
+                return self.counter
+
+        obj = TestClass()
+
+        # First call should increase counter
+        result1 = obj.method
+        # Second call should return the cached result
+        result2 = obj.method
+
+        self.assertEqual(result1, 1)
+        self.assertEqual(result2, 1)
+        self.assertEqual(obj.counter, 1)  # Confirm method was called only once
 
 
 if __name__ == '__main__':
