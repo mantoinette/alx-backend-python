68151b17346ef7670414134fe1e1ad5994ca5e74
mantoinette <152092675+mantoinette@users.noreply.github.com>
2025-07-21 16:14:05 +0200

    Update utils.py

--- 0x03-Unittests_and_integration_tests/utils.py
+++ 0x03-Unittests_and_integration_tests/utils.py
@@ -1,20 +1,10 @@
 #!/usr/bin/env python3
-"""Utility functions module"""
+""" Utility module """
 
-def access_nested_map(nested_map, path):
-    """Access nested dictionary with a list of keys."""
-    for key in path:
-        nested_map = nested_map[key]
-    return nested_map
+import requests
 
 
-def memoize(method):
-    """Decorator to cache method results."""
-    attr_name = "_{}".format(method.__name__)
-
-    @property
-    def wrapper(self):
-        if not hasattr(self, attr_name):
-            setattr(self, attr_name, method(self))
-        return getattr(self, attr_name)
-    return wrapper
+def get_json(url):
+    """GET request to the URL and return the JSON payload"""
+    response = requests.get(url)
+    return response.json()
