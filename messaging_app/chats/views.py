791dc8b2f8464444e3d5e74a615664cacb89d39f
mantoinette <antoni.kigali@gmail.com>
2025-07-26 21:59:28 +0200

    adding the model file

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,15 +1,24 @@
+from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+from .models import Message, Conversation
+from .serializers import MessageSerializer
 from .permissions import IsParticipantOfConversation
 
 class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated, IsParticipantOfConversation]
 
     def get_queryset(self):
-        # Show only messages where the user is a participant
-        return Message.objects.filter(
-            sender=self.request.user
-        ) | Message.objects.filter(
-            receiver=self.request.user
-        )
+        """
+        Return only messages where the user is a participant in the conversation.
+        """
+        return Message.objects.filter(conversation__participants=self.request.user)
+
+    def perform_create(self, serializer):
+        conversation = serializer.validated_data.get('conversation')
+
+        if self.request.user not in conversation.participants.all():
+            raise PermissionDenied("You are not a participant in this conversation.")
+
+        serializer.save(sender=self.request.user)
