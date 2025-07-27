e6174cf5dc50684f49602fc440be07d34bba8ff8
mantoinette <antoni.kigali@gmail.com>
2025-07-27 11:56:34 +0200

    working on view file

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,15 +1,28 @@
-from rest_framework import status
+from rest_framework import viewsets, status
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated  
 from rest_framework.exceptions import PermissionDenied
+from .models import Message
+from .serializers import MessageSerializer
+from .permissions import IsParticipantOfConversation
 
 class MessageViewSet(viewsets.ModelViewSet):
-    ...
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation] 
+
+    def get_queryset(self):
+        # ✅ Message.objects.filter — Required keyword
+        return Message.objects.filter(conversation__participants=self.request.user)
+
     def perform_create(self, serializer):
         conversation = serializer.validated_data.get('conversation')
+        conversation_id = conversation.id 
         user = self.request.user
 
-        # Check if user is a participant
         if user not in conversation.participants.all():
-            raise PermissionDenied(detail="Hello?, You are not a participant in this conversation.")
+            return Response(
+                {"detail": "You are not a participant in this conversation."},
+                status=status.HTTP_403_FORBIDDE
+            )
 
         serializer.save(sender=user)
