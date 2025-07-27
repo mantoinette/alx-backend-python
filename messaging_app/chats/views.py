4159a2f3cbe8f82af723bbc2d6f48addda50ea8e
mantoinette <antoni.kigali@gmail.com>
2025-07-27 12:02:15 +0200

    debuging same error on the view file

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -8,7 +8,7 @@ from .permissions import IsParticipantOfConversation
 
 class MessageViewSet(viewsets.ModelViewSet):
     serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated, IsParticipantOfConversation] 
+    permission_classes = [IsAuthenticated, IsParticipantOfConversation]  
 
     def get_queryset(self):
         # ✅ Message.objects.filter — Required keyword
@@ -16,13 +16,13 @@ class MessageViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         conversation = serializer.validated_data.get('conversation')
-        conversation_id = conversation.id 
+        conversation_id = conversation.id  #
         user = self.request.user
 
         if user not in conversation.participants.all():
             return Response(
                 {"detail": "You are not a participant in this conversation."},
-                status=status.HTTP_403_FORBIDDE
+                status=status.HTTP_403_FORBIDDEN  
             )
 
         serializer.save(sender=user)
